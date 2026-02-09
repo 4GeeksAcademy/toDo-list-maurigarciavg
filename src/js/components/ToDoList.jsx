63fca2198995f0764efee5db1354d9e0c7d41899
Mauri García <63955780+maurigarciavg@users.noreply.github.com>
2026-02-09 20:20:28 +0000

    ToDo list con fetch y API terminado

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,11 +1,91 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 
 const ToDoList = () => {
-
 	const [value, setValue] = useState("");
 	const [list, setList] = useState([]);
+	const user = "maurigarciavg";
 
+	const createUser = async () => {
+		try {
+			const response = await fetch(`https://playground.4geeks.com/todo/users/${user}`, {
+				method: "POST",
+			});
+			if (response.ok) {
+				const data = await response.json();
+				console.log("Usuario creado correctamente:", data);
+			} else {
+				console.log("Algo falló:", response.status);
+			};
+		} catch (error) {
+			console.log("Error de red:", error);
+		}
+	};
+
+	const getTasks = async () => {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/maurigarciavg");
+			if (response.ok) {
+				const data = await response.json();
+				setList(data.todos);
+				setValue("")
+				console.log("Esta es la lista de tareas actual", data.todos);
+			} else if (response.status === 404) {
+				console.log("Ha habido un error:", response.status);
+				createUser()
+			} else {
+				console.log("Ha habido un error:", response.status);
+			};
+		} catch (error) {
+			console.log("Error de red:", error);
+		};
+	};
+
+	const sendTasks = async () => {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/todos/maurigarciavg", {
+				method: "POST",
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					label: value,
+					is_done: false
+				})
+			});
+			if (response.ok) {
+				const data = await response.json();
+				getTasks()
+
+			} else {
+				console.log("Algo falló:", response.status);
+			}
+		} catch (error) {
+			console.log("Error de red:", error);
+		}
+	};
+
+	const deleteTask = async (id) => {
+		try {
+			const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+				method: "DELETE",
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			if (response.ok) {
+				getTasks()
+
+			} else {
+				console.log("Algo falló:", response.status);
+			}
+		} catch (error) {
+			console.log("Error de red:", error);
+		}
+	};
+
+	useEffect(() => {
+		getTasks();
+	}, []);
 
 	return (
 		<div className="notebook-container">
@@ -22,34 +102,22 @@ const ToDoList = () => {
 						onChange={(e) => setValue(e.target.value)}
 						onKeyDown={(e) => {
 							if (e.key === "Enter") {
-
-								setList([...list, value])
-
-								setValue("")
-
-
-								console.log("Enter pulsado")
+								sendTasks()
 							}
 						}} />
-
-
-
 					<ul>
 						{list.map((item, index) => (
 							<li key={index}>
-								<div className="listElement">{item}</div> <button className="delete" onClick={() =>
-									setList(list.filter((element) => element !== item))}>X</button> </li>
+								<div className="listElement">{item.label}</div>
+								<button className="delete" onClick={() =>
+									deleteTask(item.id)}>X</button>
+							</li>
 						))}
 					</ul>
 					<p className="todo-footer mt-auto">{list.length} tareas pendientes</p>
-
-
-
 				</div>
 			</div>
 		</div>
-
-
 	);
 };
 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
-// index.css'
 import '../styles/index.css'
 
-// components
 import ToDoList from './components/ToDoList';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
